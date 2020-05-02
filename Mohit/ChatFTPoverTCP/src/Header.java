20beb9697e167fa9b9b35d3f53ce5283ddf4b956
ashimaa123 <36802565+ashimaa123@users.noreply.github.com>
2020-05-01 23:26:45 -0500

    Update Header.java

--- Mohit/ChatFTPoverTCP/src/Header.java
+++ Mohit/ChatFTPoverTCP/src/Header.java
@@ -1,3 +1,16 @@
+/*
+ * Filename     Header.java
+ * Date         5/1/2020
+ * Author       Ashima Soni, Mira Jambusaria
+ * Email        ashima.soni@utdallas.edu mmj170530@utdallas.edu
+ * Course       CE 4390.502 Spring 2020
+ * Version      1.0
+ * Copyright    2020, All Rights Reserved
+ *
+ * Description
+ *
+ * This is the file that holds the headers that we will use to send our packets
+ */
 import java.io.Serializable;
 import java.util.Comparator;
 
