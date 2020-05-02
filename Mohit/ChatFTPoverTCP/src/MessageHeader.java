f3b219c8eb1470ce5a8a4fea6c9dc3a8de6823c6
ashimaa123 <36802565+ashimaa123@users.noreply.github.com>
2020-05-01 23:24:11 -0500

    Update MessageHeader.java

--- Mohit/ChatFTPoverTCP/src/MessageHeader.java
+++ Mohit/ChatFTPoverTCP/src/MessageHeader.java
@@ -2,18 +2,29 @@ public class MessageHeader {
     String messageType = "";
     String data = "";
 
+    /*
+    this creates a header for a message 
+    */
     MessageHeader(String x, String y){
-        if(x.equals("requestFile")){
+        //if message is to request a file
+        if(x.equals("requestFile"))
+        {
+            this.messageType = x;
+            this.data = y; 
+        } 
+        //if message is to send a chat
+        else if (x.equals("sendChat"))
+        {
             this.messageType = x;
             this.data = y;
-        } else if (x.equals("sendChat")){
-            this.messageType = x;
-            this.data = y;
-        }  else {
-            System.out.println("Invalid Header Message Type.");
+        } 
+        else 
+        {
+            System.out.println("This header message type is invalid.");
         }
     }
 
+    //return the message type and the data 
     String messageToString(){
         return(messageType + " " + data);
     }
