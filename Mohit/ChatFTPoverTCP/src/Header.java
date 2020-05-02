fd5301a4e12981918d3fa56baf853d192b7a53a9
ashimaa123 <36802565+ashimaa123@users.noreply.github.com>
2020-05-01 22:39:46 -0500

    Update Header.java

--- Mohit/ChatFTPoverTCP/src/Header.java
+++ Mohit/ChatFTPoverTCP/src/Header.java
@@ -6,36 +6,30 @@ public class Header implements Serializable
     private static final long serialVersionUID = 1L;
 
     byte[] payload = null;
-    long num = 0;
+    long number = 0;
     String checksum = null;
 
     /*
-     * Creates a header object that contains a byte array, a sequence number and
-     * checksum string
+     * header object with a byte array, a sequence number, checksum string
      */
     public Header(byte[] data, long sequence, String chksm)
     {
         payload = data;
-        num = sequence;
+        number = sequence;
         checksum = chksm;
     }
 
     /*
-     * Allows the header object to be compares to other header object. This
-     * helps in sorting a data-structure full of header objects. To compare two
-     * header objects the method checks the sequence number of each object and
-     * returns the difference in the sequence numbers. If the the returned
-     * number is negative that means the second object has a larger sequence
-     * number than the first.
+     * Compared one header object with another header object by taking the difference between the two
      */
     public static Comparator<Header> compareheader = new Comparator<Header>()
     {
         public int compare(Header h1, Header h2)
         {
-            int num1 = (int) h1.num;
-            int num2 = (int) h2.num;
+            int number1 = (int) h1.number;
+            int num2 = (int) h2.number;
 
-            return (num1 - num2);
+            return (number1 - number2);
         }
     };
 }
